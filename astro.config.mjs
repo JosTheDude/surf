18d146b3bf71ae36277acffd90fdf80a01121c33
Jos (BM) <43651265+JosTheDude@users.noreply.github.com>
2022-10-26 09:21:11 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress()]
+  integrations: [compress()],
+  sitemap: true,
+  site: 'https://surf.joscodes.xyz/',
+  outDir: 'public',
+  publicDir: 'static'
 });
